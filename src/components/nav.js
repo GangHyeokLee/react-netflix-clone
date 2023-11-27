3e30de0a5dd3a4f9c7ae0cb4d9864b9bc52a9dc2
GangHyeok Lee <rkdgur5381@gmail.com>
2023-11-27 22:02:25 +0900

    Search & Modal Close Feature

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,37 +1,50 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import "./nav.css"
+import { useNavigate } from "react-router-dom";
 
 export default function Nav() {
-    const [show, setshow] = useState(false);
+    const navigate = useNavigate();
 
-    useEffect(()=>{
-        window.addEventListener("scroll", ()=>{
-            if(window.scrollY > 50){
+    const [show, setshow] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 50) {
                 // console.log(window.scrollY)
                 setshow(true);
-            }else{
+            } else {
                 setshow(false);
             }
         })
 
-        return () =>{
-            window.removeEventListener("scroll", () =>{});
+        return () => {
+            window.removeEventListener("scroll", () => { });
         }
     })
 
-  return (
-    <nav className={`nav ${show && "nav__black"}`}>
-        <img
-        alt='Netflix Logo'
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2880px-Netflix_2015_logo.svg.png"
-        className="nav__logo"
-        onClick={() => window.location.reload()}
-        />
-        <img
-            alt="User logged"
-            src="https://occ-0-1360-2218.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABcTfDTM3oHx4rFqndqkNITU7rFipT1w0emptYNA0JKiToaRWZ6tlLSMOqjXhlAMBd74YgXLYZxNfFKWInrL8ndVLMxS-1Y0.png?r=64f"
-            className="nav__avatar"
-        />
-    </nav>
-  )
+    const handleChange = (e) => {
+        setSearchValue(e.target.value);
+        navigate(`/search?q=${e.target.value}`);
+
+    };
+
+    return (
+        <nav className={`nav ${show && "nav__black"}`}>
+            <img
+                alt='Netflix Logo'
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2880px-Netflix_2015_logo.svg.png"
+                className="nav__logo"
+                onClick={() => window.location.reload()}
+            />
+
+            <input value={searchValue} onChange={handleChange} className="nav__input" type="text" placeholder="영화를 검색해주세요." />
+
+            <img
+                alt="User logged"
+                src="https://occ-0-1360-2218.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABcTfDTM3oHx4rFqndqkNITU7rFipT1w0emptYNA0JKiToaRWZ6tlLSMOqjXhlAMBd74YgXLYZxNfFKWInrL8ndVLMxS-1Y0.png?r=64f"
+                className="nav__avatar"
+            />
+        </nav>
+    )
 }
